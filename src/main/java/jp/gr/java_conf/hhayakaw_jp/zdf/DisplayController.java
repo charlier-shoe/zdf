71ecfeac8e7095588452e3b31b5f0d4fd07a6d86
charlier-shoe <charlier.shoe@gmail.com>
2016-06-12 23:40:07 +0900

    Minor refactoring.

--- src/main/java/jp/gr/java_conf/hhayakaw_jp/zdf/DisplayController.java
+++ src/main/java/jp/gr/java_conf/hhayakaw_jp/zdf/DisplayController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  *
  */
 @Controller
-public class IndexController {
+public class DisplayController {
 
     @RequestMapping("/")
     public String showTopPage() {
